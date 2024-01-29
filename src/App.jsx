51505314e91f34f0639f1581c302e738850a1d08
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-01-29 21:51:16 +0500

    File setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Products from "./Products";
+import Contact from "./Contact";
+import SinglePage from "./SinglePage";
+import Cart from "./Cart";
+import ErrorPage from "./ErrorPage";
+
 function App() {
-  return <div>hello g</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/singleproduct" element={<SinglePage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
