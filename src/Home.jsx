16f3b36ce6a55624f49aa74d67af21d49faaf899
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-01-30 20:10:32 +0500

    some issues in services solved

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
 import { Services } from "./components/Services.JSX";
+import Trusted from "./components/Trusted";
 
 const Home = () => {
   return (
     <div>
       <HeroSection>Abdul E-commerce</HeroSection>
       <Services />
+      <Trusted />
     </div>
   );
 };
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,28 +6,36 @@ import { RiSecurePaymentLine } from "react-icons/ri";
 
 export const Services = () => {
   return (
-    <section className=" mb-[6rem]">
+    <section className=" mt-[10rem]">
       <div className=" grid grid-cols-3 gap-5  max-w-[60rem] m-auto">
         {/* ARTICLE-1  =========> */}
         <article className="flex flex-col gap-4 items-center justify-center h-[18rem] bg-slate-200 rounded-md">
-          <FaTruck className="text-[2.5rem] text-amber-500" />
+          <div className="w-[4.5rem] h-[4.5rem] bg-[white] grid place-content-center rounded-[50%]">
+            <FaTruck className="text-[2.5rem] text-amber-500" />
+          </div>
           <p className="capitalize font-bold ">super fast and free delivery</p>
         </article>
         {/* ARTICLE-2  =========> */}
         <article className="grid grid-rows-2 gap-4">
-          <div className="bg-slate-200 flex flex-col gap-2  justify-center items-center rounded-md">
-            <MdOutlineSecurity className="text-[2.5rem] text-amber-500" />
+          <div className="bg-slate-200 flex  gap-2  justify-center items-center rounded-md">
+            <div className="w-[4.5rem] h-[4.5rem] bg-[white] grid place-content-center rounded-[50%]">
+              <MdOutlineSecurity className="text-[2.5rem] text-amber-500" />
+            </div>
             <p className="capitalize font-bold ">Non-contract Shipping</p>
           </div>
-          <div className="bg-slate-200 flex flex-col gap-2  justify-center items-center rounded-md">
-            <MdOutlineSecurity className="text-[2.5rem] text-amber-500" />
-            <p className="capitalize font-bold ">Non-contract Shipping</p>
+          <div className="bg-slate-200 flex  gap-2   justify-center items-center rounded-md">
+            <div className="w-[4.5rem] h-[4.5rem] bg-[white] grid place-content-center rounded-[50%]">
+              <GiReceiveMoney className="text-[2.5rem] text-amber-500" />
+            </div>
+            <p className="capitalize font-bold ">money-back guaranteed</p>
           </div>
         </article>
-        {/* ARTICLE-1  =========> */}
+        {/* ARTICLE-3  =========> */}
         <article className="flex flex-col gap-4 items-center justify-center h-[18rem] bg-slate-200 rounded-md">
-          <FaTruck className="text-[2.5rem] text-amber-500" />
-          <p className="capitalize font-bold ">super fast and free delivery</p>
+          <div className="w-[4.5rem] h-[4.5rem] bg-[white] grid place-content-center rounded-[50%]">
+            <RiSecurePaymentLine className="text-[2.5rem] text-amber-500" />
+          </div>
+          <p className="capitalize font-bold ">super secure payment system</p>
         </article>
       </div>
     </section>
