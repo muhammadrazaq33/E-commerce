0302ccbdde9e6456c178c4b92c00805ceb0e86e7
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-02-03 21:57:59 +0500

    Error Page Completed

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <section>
+      <div className="grid place-content-center">
+        <div className=" text-center flex flex-col gap-4">
+          <h1 className=" text-[4rem] font-bold leading-[3.2rem]">404</h1>
+          <h3 className=" text-[2rem] leading-[2rem]">UH HO! You're lost.</h3>
+          <p className=" text-[rem]">
+            The page you are looking for does not eXisT. How you get here is
+            mystery. But you can click the button below to go back to the
+            homepage.
+          </p>
+          <NavLink to="/">
+            <button className=" capitalize bg-amber-400 px-6 py-3">
+              GO TO HOME PAGE
+            </button>
+          </NavLink>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ErrorPage;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ import { FaYoutube } from "react-icons/fa";
 
 export const Footer = () => {
   return (
-    <footer className="my-[5rem]">
+    <footer className="mt-[5rem]">
       {/* FOOTER Title ===========> */}
-      <div className=" flex justify-between items-center max-w-[60rem] m-auto bg-slate-200 px-[3.8rem] py-8 rounded-lg">
+      <div className=" flex justify-between items-center max-w-[60rem] m-auto bg-slate-200 px-[3.8rem] py-8 rounded-lg translate-y-12">
         <div className="text-[1.1rem] font-medium">
           <h3>Ready to get started</h3>
           <h3>Talk to us today</h3>
@@ -21,7 +21,7 @@ export const Footer = () => {
       </div>
 
       {/* Main FOOTER  ===========> */}
-      <div className=" bg-blue-950 py-[5rem]">
+      <div className=" bg-blue-950 pt-[5rem]">
         <div className=" max-w-[60rem] m-auto grid md:grid-cols-4 grid-cols-2 gap-y-10 lg:gap-x-[3rem] gap-x-[2.5rem] px-6">
           {/* ABOUT ARTICLE-1  =========> */}
           <article className=" text-[white] flex flex-col gap-3.5">
@@ -70,8 +70,26 @@ export const Footer = () => {
           {/* CONTACT FOOTER-4 */}
           <article className="font-semibold text-[white] flex flex-col gap-4">
             <h3>CONTACT US</h3>
-            <p>+337-6725198</p>
+            {/* <p>+337-6725198</p> */}
+            <a href="tel:3324030947">+92-3324030947</a>
           </article>
+        </div>
+        {/* FOOTER BOTTOM ==========> */}
+        <div className="w-full h-[0.1rem] bg-white mt-[4.5rem]">J</div>
+        <div className="mt-[2rem] max-w-[60rem] m-auto px-6">
+          <div className="text-white flex sm:flex-row sm:justify-between flex-col justify-center gap-x-[2rem] gap-y-[1rem] sm:items-start items-center">
+            <p className=" hover:text-amber-400 cursor-pointer md:max-w-[23rem] max-w-[25rem]">
+              @{new Date().getFullYear()} ABDUL E-commerce. All Rights Reserved
+            </p>
+            <div className=" flex flex-col gap-1 sm:items-start items-center">
+              <p className=" hover:text-amber-400 cursor-pointer">
+                Privacy Policy
+              </p>
+              <p className=" hover:text-amber-400 cursor-pointer">
+                Terms & Conditions
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
