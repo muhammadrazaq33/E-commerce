c6c21555db8b265548d8174f553d6919b18eb76d
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-02-13 20:23:28 +0500

    CartAmountToggle Completed

--- src/components/cards/AddToCart.jsx
+++ src/components/cards/AddToCart.jsx
@@ -1,30 +1,59 @@
 import React, { useState } from "react";
 import { FaCheck } from "react-icons/fa";
+import CartAmountToggle from "./CartAmountToggle";
+import { NavLink } from "react-router-dom";
 
 const AddToCart = ({ products }) => {
   const { id, stock, colors } = products;
   const [color, setColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+
+  // Increasing function
+  const setIncrease = () => {
+    stock > amount ? setAmount(amount + 1) : setAmount(1);
+  };
+
+  // Decreasing function
+  const setDecrease = () => {
+    amount > 1 ? setAmount(amount - 1) : setAmount(amount);
+  };
 
   return (
-    <div>
-      <p className=" flex items-center gap-2">
-        {" "}
-        Colors:
-        {colors.map((curCol, index) => {
-          return (
-            <button
-              key={index}
-              style={{ backgroundColor: curCol }}
-              className={`w-[1.6rem] h-[1.6rem] grid place-content-center rounded-[50%] ${
-                color === curCol ? "active" : "faded"
-              }`}
-              onClick={() => setColor(curCol)}
-            >
-              {color === curCol ? <FaCheck className="text-[white]" /> : null}
-            </button>
-          );
-        })}
-      </p>
+    <div className=" flex flex-col gap-4">
+      <div>
+        <p className=" flex items-center gap-2">
+          {" "}
+          Colors:
+          {colors.map((curCol, index) => {
+            return (
+              <button
+                key={index}
+                style={{ backgroundColor: curCol }}
+                className={`w-[1.6rem] h-[1.6rem] grid place-content-center rounded-[50%] ${
+                  color === curCol ? "active" : "faded"
+                }`}
+                onClick={() => setColor(curCol)}
+              >
+                {color === curCol ? <FaCheck className="text-[white]" /> : null}
+              </button>
+            );
+          })}
+        </p>
+      </div>
+
+      {/* ADD TO CART  ===========>*/}
+      <CartAmountToggle
+        amount={amount}
+        setIncrease={setIncrease}
+        setDecrease={setDecrease}
+      />
+
+      {/* BUTTON  =======> */}
+      <NavLink to="/cart">
+        <button className=" px-5 py-3 bg-amber-400 font-medium">
+          ADD TO CART
+        </button>
+      </NavLink>
     </div>
   );
 };
---
+++ src/components/cards/CartAmountToggle.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { FaMinus, FaPlus } from "react-icons/fa";
+
+const CartAmountToggle = ({ amount, setIncrease, setDecrease }) => {
+  return (
+    <div className=" flex items-center gap-8 ml-1">
+      <button className="text-[1.2rem]" onClick={() => setDecrease()}>
+        <FaMinus />{" "}
+      </button>
+      <p className=" text-[1.2rem]">{amount}</p>
+      <button className="text-[1.2rem]" onClick={() => setIncrease()}>
+        <FaPlus />{" "}
+      </button>
+    </div>
+  );
+};
+
+export default CartAmountToggle;
