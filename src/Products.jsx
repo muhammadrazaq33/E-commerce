96adf3770ad11d83f9625bac71bf837bd568bc8c
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-02-14 14:12:30 +0500

    Products Page Setup Completed

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import FilterSection from "./components/cards/FilterSection";
+import Sort from "./components/cards/Sort";
+import ProductList from "./components/cards/ProductList";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <>
+      <div className="TWO_COL max-w-[60rem] m-auto">
+        {/* FILTERSECTION   ======> */}
+        <div>
+          <FilterSection />
+        </div>
+
+        {/* SORT & LIST =====> */}
+        <div className=" grid grid-rows-2">
+          {/* SORT ========>*/}
+          <div>
+            <Sort />
+          </div>
+
+          {/* LIST ========> */}
+          <div>
+            <ProductList />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Products;
